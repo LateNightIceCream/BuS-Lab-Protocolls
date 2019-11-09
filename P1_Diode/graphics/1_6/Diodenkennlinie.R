a87f9ebb76ce5342784be07be368363f9ea09175
LateNightIceCream <richardgruenert@live.com>
2019-11-09 12:44:01 +0100

    update!

--- P1_Diode/graphics/1_6/Diodenkennlinie.R
+++ P1_Diode/graphics/1_6/Diodenkennlinie.R
@@ -8,12 +8,18 @@ outputName   <- "diodenkennline.pdf"
 outputWidth  <- 10
 outputHeight <- 0.618 * outputWidth
 
-UT <- 25 * 10^(-3)
+UT <- 2.2 * 26 * 10^(-3)
 n  <- 1
-Is <- 1 * 10^(-6)
+Is <- 0.15 * 10^(-6)
 IsArtificial <- -0.02
 
 ZenerVoltage <- -1
+
+power_hyperbola <- function (U) {
+
+    1.5/U
+
+}
 
 schockley <- function(U) {
 
@@ -23,16 +29,20 @@ schockley <- function(U) {
 
 forwardBias <- function(U) {
 
-    ifelse(U<ZenerVoltage, -schockley(-(U+0.9))+IsArtificial, ifelse(U >= 0, schockley(U), IsArtificial))
+    #ifelse(U<ZenerVoltage, -schockley(-(U+0.9))+IsArtificial, ifelse(U >= 0, schockley(U), IsArtificial))
+    schockley(U)
 }
 
 xlabel <- "U / V"
 ylabel <- "I / A"
 
-xlim <- c(-1.5,1)
-ylim <- c(-1,1)
+xlim <- c(0,0.9)
+ylim <- c(0,schockley(0.9))
+
+xbreaks <- seq(0,0.9, by=0.1)
 
 function_color <- "#00b1db"
+power_color <- "#00b1db0F"
 
 ####################################################################
 
@@ -43,14 +53,15 @@ p <- ggplot(data.frame(x=c(-2,2), y=c(-2,2)), aes(x=x)) +
         #ggtitle("x(t)") +
 
     stat_function( fun = forwardBias,n=2000, color = function_color, size = 1) +
+    stat_function( fun = power_hyperbola,n=2000, color = power_color, size = 1) +
 
         theme(text = element_text(size=16.180339887498948), axis.title.y = element_text(angle = 0, vjust = .5), panel.grid.minor.x = element_blank()) + # rotate axis title
 
         xlab(xlabel) +
         ylab(ylabel)  +
 
-        scale_y_continuous(limits=c(-1.618033, 1.6180339887498948)) +
-        scale_x_continuous(limits=xlim)
+        scale_y_continuous(limits=ylim) +
+        scale_x_continuous(limits=xlim, breaks=xbreaks)
 
 
 pdf(outputName, width = outputWidth, height = outputHeight)
