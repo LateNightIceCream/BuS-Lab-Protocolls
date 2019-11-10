2bdd8c6b6dba73585307985421f81a87b65dec0c
LateNightIceCream <richardgruenert@live.com>
2019-11-10 15:57:55 +0100

    update

--- P1_Diode/graphics/2_5/diode.R
+++ P1_Diode/graphics/2_5/diode.R
@@ -1,17 +1,17 @@
 library(ggplot2)
 
-outputName = "2_4_kennlinie.pdf"
+outputName = "2_5.pdf"
 outputWidth = 10
 outputHeight = outputWidth * 0.618
 
-diodeDataFB <- read.csv("2_4.csv")
+diodeDataFB <- read.csv("2_5.csv")
 
-xlabel <- bquote("U"[e]~"/V")
+xlabel <- bquote("I"[L]~"/mA")
 ylabel <- bquote("U"[a]~"/V")
                                         #
 hsBlue <- "#00b1db"
 
-plot <- ggplot( data= diodeDataFB, aes(x = Ue, y = Ua)) +
+plot <- ggplot( data= diodeDataFB, aes(x = IL * 1000, y = Ua)) +
     theme_minimal() +
     xlab(xlabel) +
     ylab(ylabel) +
