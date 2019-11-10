0a54ee6633a4b7dd87103f819055df3ccb63f49a
LateNightIceCream <richardgruenert@live.com>
2019-11-10 20:47:02 +0100

    final update?

--- P1_Diode/graphics/2_2/diode.R
+++ P1_Diode/graphics/2_2/diode.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-outputName = "2_3_kennlinie.pdf"
+outputName = "2_2_kennlinie.pdf"
 outputWidth = 10
 outputHeight = outputWidth * 0.618
 
@@ -10,16 +10,28 @@ diodeDataRB <- read.csv("sperrrichtung.csv")
 xlabel <- "U / V"
 ylabel <- "I / mA"
 
+powerloss <- function(U) {
+
+    1.5/U * 1000
+
+}
                                         #
 hsBlue <- "#00b1db"
 hsInvert <- "#ff5d02"
+hsGreen <- "#59B574"
+hsGreenfade <- "#59B5745E"
+
+ylimits <- c(min(diodeDataRB), max(diodeDataFB))
 
 plot <- ggplot( data= diodeDataFB, aes(x = diodeDataFB$U, y = diodeDataFB$I)) +
     theme_minimal() +
-    xlab(xlabel) + 
-    ylab(ylabel) + 
+    xlab(xlabel) +
+    ylab(ylabel) +
     geom_point(color=hsBlue) +
-    geom_point(color=hsInvert,data=diodeDataRB, aes(x = diodeDataRB$U, y = diodeDataRB$I))
+    geom_point(color=hsInvert,data=diodeDataRB, aes(x = diodeDataRB$U, y = diodeDataRB$I))+
+    scale_y_continuous(limits=ylimits) +
+    stat_function(fun = powerloss, color = hsGreen) +
+    annotate("text", x=-4.6, y=-268, label="Verlustleistungshyperbel", size=04, color = hsGreenfade)
 
 pdf(outputName, width = outputWidth, height = outputHeight)
 plot
